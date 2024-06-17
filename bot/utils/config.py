a419a6c9cbc558a2170fddc59d035173f5197eb2
icebergD <snfmxmsc.s@gmail.com>
2024-06-17 18:10:33 +0500

    changed

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -3,7 +3,7 @@ from .singleton import Singleton
 
 class config(Singleton):
     MAIN_TOKEN = ''
-    TEST_TOKEN = '7309028858:AAG4cAYbMMtnXcOaRZuv45nY3eICWBkXs1s'
+    TEST_TOKEN = ''
     TOKEN = TEST_TOKEN
 
     DISABLE_WEB_PAGE_PREVIWE = True
